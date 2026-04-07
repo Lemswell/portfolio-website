71be52329ba4410615d9b169b6012526cd8c0508
Lemswell <42715753+Lemswell@users.noreply.github.com>
2026-04-07 15:06:10 +1000

    Enhanced github API implementation

--- lib/github.ts
+++ lib/github.ts
@@ -1,15 +1,18 @@
-
 import type { GithubRepository } from "@/types/github";
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN; // not sure how to not have this go through if it cant find the variable
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN; 
+
+const headers: { [key: string]: string } = {
+    "Accept": "application/vnd.github+json",
+}
+
+if (GITHUB_TOKEN != undefined) 
+    headers["Authorization"] = `Bearer ${GITHUB_TOKEN}`;
 
 export async function fetchRepos(): Promise<GithubRepository[]> {
     // method default to GET
     const response = await fetch("https://api.github.com/users/Lemswell/repos", { 
-        headers: {
-            "Authorization": `Bearer ${GITHUB_TOKEN}`,
-            "Accept": "application/vnd.github+json"
-        }
+        headers: headers
     });
     
     if (!response.ok) {
@@ -19,5 +22,3 @@ export async function fetchRepos(): Promise<GithubRepository[]> {
     const data = await response.json() as GithubRepository[];
     return data;
 }
-
-
