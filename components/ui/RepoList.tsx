3b6523f34e9bd4c2c180672bc8702832ba8dcd2e
Lemswell <42715753+Lemswell@users.noreply.github.com>
2026-04-16 13:16:06 +1000

    Working on generating dynamic list of displayed repos

--- components/ui/RepoList.tsx
+++ components/ui/RepoList.tsx
@@ -1,11 +1,18 @@
 import { fetchRepos } from "@/lib/github";
 
-async function RepoList() {
-  
+async function RepoList(displayLim?: number, timeLim?: number) { 
+
   const repos = await fetchRepos()
   const sortedRepos = repos.sort((a, b) => new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime());
 
+
+
+  // maybe add a 'no show' list
+  for (let i: number = 0; i < (displayLim ? displayLim : repos.length); i++) {
+    
+  }
   
+
 }
 
-export default RepoList
+export { RepoList };
