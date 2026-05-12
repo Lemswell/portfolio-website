eb67b61cfac8ea892efb3e57ef842942403493b2
Lemswell <42715753+Lemswell@users.noreply.github.com>
2026-05-12 12:39:04 +1000

    Touched up styling on blog and project page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,11 +4,16 @@ export default function App() {
   return (
     <main className="max-w-3xl mx-auto px-6 py-20">
       <header className="my-8">
+        <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-blue-950 dark:text-blue-400 flex items-center line-clamp-1">
+        <a href="/" className="text-white hover:text-blue-900 dark:hover:text-blue-300 transition-colors">
+          {"Lems'\u00A0"}
+        </a>
+      </h1>
         <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 flex items-center line-clamp-1">
           Blog
         </h1>
       </header>
-      <hr className='border-black/10 dark:border-white/10'></hr>
+      <hr className='my-8 border-black/10 dark:border-white/10'></hr>
       < BlogPostList />
     </main>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,9 +5,17 @@ import { fetchRepos } from "@/lib/github";
 export default async function App() {
   const repos = await fetchRepos();
   return (
-    <main>
-      <h1>Posts</h1>
-      < RepoList repos={repos} displayLim={5} />
+    <main className="max-w-3xl mx-auto px-6 py-20">
+      <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-blue-950 dark:text-blue-400 flex items-center line-clamp-1">
+        <a href="/" className="text-white hover:text-blue-900 dark:hover:text-blue-300 transition-colors">
+          {"Lems'\u00A0"}
+        </a>
+      </h1>
+      <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 flex items-center line-clamp-1">
+        Projects
+      </h1>
+      <hr className='my-8 border-black/10 dark:border-white/10'></hr>
+      < RepoList repos={repos} />
     </main>
   );
 }
