3549d0da71aee3f06d2219846852ebdb2b27b1be
Lemuel <42715753+Lemswell@users.noreply.github.com>
2026-06-15 13:11:54 +1000

    added 'featured only' list for projects

--- config/github.ts
+++ config/github.ts
@@ -2,4 +2,9 @@ export const BLACKLISTED_REPO_FULLNAMES: string[] = [
   "Lemswell/Lemswell",
 ];
 
+export const FEATURED_REPOS: string[] = [
+  "Lemswell/portfolio-website",
+  "Lemswell/tasker",
+];
+
 export const USERNAME = "Lemswell";
