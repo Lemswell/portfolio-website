f27948a30e2a4c9aea8180eac1b71d34384a1f0e
Lemuel <42715753+Lemswell@users.noreply.github.com>
2026-06-22 15:40:52 +1000

    temp solution for md outside links redirecting

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,20 +1,48 @@
-import rehypeStringify from 'rehype-stringify'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import {unified} from 'unified'
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 
-
-export async function compileMarkdown(rawMarkdownString: string): Promise<string> {
-    const file = await unified()
+export async function compileMarkdown(
+  rawMarkdownString: string,
+): Promise<string> {
+  const file = await unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeStringify)
-    .process(rawMarkdownString).catch((e) => {
-        console.error("Error compiling markdown:", e);
-        return null; // return null if there's an error during compilation
+    .process(rawMarkdownString)
+    .catch((e) => {
+      console.error("Error compiling markdown:", e);
+      return null; // return null if there's an error during compilation
     });
-    
-    const compiledContent = String(file);
 
-    return compiledContent;
+  const compiledContent = outsideLinkFormatter(String(file));
+
+  return compiledContent;
+}
+
+export function outsideLinkFormatter(compiledMarkdown: string): string {
+  let compiledContent: string = compiledMarkdown;
+
+  let index_of_last_closing_bracket: number = -1;
+  for (let i = compiledContent.length - 1; i >= 0; i--) {
+    if (compiledContent[i] === `>`) {
+      index_of_last_closing_bracket = i;
+    }
+    if (i + 10 <= compiledContent.length) {
+      if (compiledContent.substring(i, i + 10) === `href="http`) {
+        // for every outside link
+        if (index_of_last_closing_bracket !== -1) {
+          compiledContent =
+            compiledContent.substring(0, index_of_last_closing_bracket) +
+            ` target="_blank" rel="noopener noreferrer"` +
+            compiledContent.substring(index_of_last_closing_bracket);
+          index_of_last_closing_bracket = -1;
+        }
+      }
+    }
+  }
+  // target="_blank" rel="noopener noreferrer"
+
+  return compiledContent;
 }
